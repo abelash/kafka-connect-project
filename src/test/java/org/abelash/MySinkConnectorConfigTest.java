0cf0242cdfd37fbc9687b14b125c8f9b22693e9d
abelash <abelash@gmail.com>
2019-12-16 16:36:42 +0200

    Initial commit

---
+++ src/test/java/org/abelash/MySinkConnectorConfigTest.java
@@ -0,0 +1,10 @@
+package org.abelash;
+
+import org.junit.Test;
+
+public class MySinkConnectorConfigTest {
+  @Test
+  public void doc() {
+    System.out.println(MySinkConnectorConfig.conf().toRst());
+  }
+}
